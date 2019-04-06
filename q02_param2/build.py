75165032e2525b5abb063ddea3a0f465498450ad
Sarang Narkhede <narkhedesarang@gmail.com>
2019-04-06 15:48:12 -0400

    Done

--- q02_param2/build.py
+++ q02_param2/build.py
@@ -4,9 +4,7 @@ from xgboost import XGBClassifier
 import pandas as pd
 from greyatomlib.Xgboost_project.q01_myXGBoost.build import myXGBoost
 
-# load data
 dataset = pd.read_csv('data/loan_clean_data.csv')
-# split data into X and y
 X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=9)
@@ -16,7 +14,6 @@ param_grid2 = {"gamma": [0, 0.05, 0.1, 0.3, 0.7, 0.9, 1],
                "reg_lambda": [0.05, 0.1, 0.5, 1.0]
                }
 xgb = XGBClassifier(seed=9)
-# Write your solution here :
 def param2(X_train,X_test,y_train,y_test,model,param_grid):
     kwargs = {'subsample': 0.8, 'colsample_bytree': 0.7, 'max_depth': 2, 'min_child_weight': 4}
     acc, best_params = myXGBoost(X_train,X_test,y_train,y_test,model,param_grid,**kwargs)
