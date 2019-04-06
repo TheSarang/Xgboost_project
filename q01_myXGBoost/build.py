ae0974822b9746615d81fafde5c446c608aac79c
Sarang Narkhede <narkhedesarang@gmail.com>
2019-04-06 15:47:02 -0400

    Update build.py

--- q01_myXGBoost/build.py
+++ q01_myXGBoost/build.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 
-# load data
 dataset = pd.read_csv('data/loan_clean_data.csv')
 # split data into X and y
 X = dataset.iloc[:, :-1]
@@ -16,7 +15,6 @@ param_grid1 = {"max_depth": [2, 3, 4, 5, 6, 7, 9, 11],
                "subsample": [0.6, .7, .8, .9, 1],
                "colsample_bytree": [0.6, .7, .8, .9, 1]
                }
-
 
 xgb = XGBClassifier(seed=9)
 def myXGBoost(X_train,X_test,y_train,y_test,model,param_grid,Kfold=3,**kwargs):
